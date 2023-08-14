7819ef1a6117db66475da519c7297402cbcb0eec
Amanda Aragão <amandaaragao72@gmail.com>
2023-08-14 18:08:28 -0300

    Requisito 5, middlewares para a valiação de name e price refente a rota products.

--- src/routers/productsRouter.ts
+++ src/routers/productsRouter.ts
@@ -2,10 +2,18 @@ import { Router } from 'express';
 import productsController from '../controllers/productsController';
 import loginController from '../controllers/loginController';
 import validateToken from '../middlewares/validateToken';
+import validadeProducts from '../middlewares/validadeProducts';
 
 const productsRouter = Router();
 
-productsRouter.post('/products', productsController.addNewProduct);
+productsRouter.post(
+  '/products', 
+  validadeProducts.validateName,
+
+  validadeProducts.validatePrice,
+
+  productsController.addNewProduct,
+);
 productsRouter.get('/products', productsController.listAllProduct);
 productsRouter.post('/login', validateToken, loginController.userLogin);
 
